14a50b7950de502eb8a951ec5b582ddbfb2ed776
RandallStevenson <59617625+RandallStevenson@users.noreply.github.com>
2020-05-09 14:10:56 -0500

    Add files via upload

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,58 +13,77 @@ var ids=sample_data.names;
 var metadata=sample_data.metadata;
 var samples=sample_data.samples;
 
-// Generate Dropdown to select individual
+// Return inddex of selected individual from dropdown
+d3.selectAll("#selDataset").on("change",updatePlotly);
 
 
-// Return selected individual
-selid = 940;
+function updatePlotly() {
+    // get the index of the selection labeled 'index_v'
+    var selection=d3.selectAll("#selDataset");
+    var index_v=selection.property("value");
+    console.log(index_v);
+    // get demographic metadata from index_v a dictionary with keys
+    demog=metadata[index_v];
+    // set sample data from index_v
+    samp_dat=samples[index_v];
 
-// Sort top 10 OTUs from selected individual
+    // get sample values from sample data
+    sam_vals=samp_dat.samplevalues;
+    // sort otu_ids, otu_lables and samplevalues by sorting samplevalues
+    // sort sample values and slice at 10
+    var sort_10=sam_vals.sort((a,b) => b-a);
+    sort_10.slice(0,10);
 
-
-
-if samples.id===selid {
-    var otu_ids=
-};
-
-// The otu count data is already in descending order, so a slice is all that is really needed.
-
-otu_sliced=otu_ids.sort().reverse().slice(0,10);
-
-// otu_var.sort().slice(0,10);
-
-// Define
-// sample_values as values
-// out_ids as labels
-// otu_lables as hovertext
+    //loop through sam_vals array to get indices for otu_ids and otu_names
+    var otu_op=[]
+    var otu_name=[]
+    console.log(sort_10)
+    for (i=0; i<sort_10.length; i++) {
+        for (j=0; j<sam_vals.length; j++) {
+            if (sam_vals[j]===sort_10[i]) {
+                otu_op.push(samples.otu_ids[j]);
+                otu_name.push(samples.out_lables[j]);
+            }
+        }
+    }
+    console.log(otu_op)
+    console.log(otu_name)
 
 // Create horizontal bar chart using plotly
-var trace1={
-x:
-y:
-text:
-name:
-type="bar"
-oreintation="h"
-}
+    var trace1={
+        x: sort_10
+        y: otu_op
+        text: otu_name
+        name: "Top 10 OTUs for Sample"
+        type="bar"
+        orientation="h"
+    };
 
-data=[trace1];
-// Use all the OTUs from selected individual
-// Define
-// otu_ids for x values
-// sample_values for y values
-// sample_values for the marker size
-// otu_ids for marker colors.
-// otu_labels for the text values
+    var data=[trace1];
 
-Plotly.newPlot("Plot",data);
+
+    Plotly.newPlot("bar",data);
 
 // Create bubble chart
 
+    var trace2={
+        x: samples.otu_ids
+        y: samples.samplevalues
+        mode: "markers"
+        marker:{ 
+            size= sample.samplevalues,
+            color=sample.otu_ids
+        }
+    };
+
+    data=[trace2]
+    Plotly.newPlot("bubble",data);
+
 // Display sample metadata - individual's demographic information
 
-// Display key-value pairs from metadata JSON object on page
 
+
+}
 // Update plots when a new sample is selected
 d3.selectAll("body").on("change",updatePage);
 
